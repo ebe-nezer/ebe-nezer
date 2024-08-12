10c261956a5542b6c5e8f9fa818c7d4f4559dfaf
Ebenezer <ebenezerebi500@gmail.com>
2024-08-12 20:17:11 +0530

    Added Navbar for mobile resonsive

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,9 @@ import CustomTypography from "@components/typography";
 import { Paper } from "@mui/material";
 import zIndex from "@mui/material/styles/zIndex";
 import { styled } from "@mui/system";
-
+import { useState } from "react";
+import { MdMenu, MdClose } from "react-icons/md";
+import { AiOutlineMenu } from "react-icons/ai";
 const options = [
   {
     route: "/",
@@ -24,19 +26,81 @@ const options = [
 ];
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <Container variant="outlined">
       <LogoContainer>
         <img src="/vite.svg" alt="logo" />
       </LogoContainer>
+      <Navbar>
+        <AiOutlineMenu
+          size={"28px"}
+          cursor={"pointer"}
+          color="grey"
+          onClick={() => setShowMenu(true)}
+        />
+      </Navbar>
+      <NavbarOptions
+        sx={{
+          // "&:hover": {
+          // ".MuiTypography-root": {
+          //   opacity: 0.3,
+          //   transition: "all .2s",
+          // },
+          opacity: showMenu ? 1 : 0,
+          zIndex: showMenu ? zIndex.modal * 100 : -1,
+          transform: showMenu ? "translateX(0px)" : "translateX(100%)",
+          // },
+          transition: "all .2s",
+        }}
+      >
+        <div className="header__button__close">
+          <MdClose
+            size={"28px"}
+            color="grey"
+            cursor={"pointer"}
+            onClick={() => setShowMenu(false)}
+          />
+        </div>
+        <div className="options__container">
+          {options.map((e) => {
+            const id = e.route.split("/")[1] || "home";
+            return (
+              <CustomButton
+                id={id}
+                // disabled={id !== "home"}
+                sx={{
+                  margin: "12px 0px",
+                  cursor: id !== "home" ? "not-allowed" : "pointer",
+                  [`&#${id}:hover`]: {
+                    ".MuiTypography-root": {
+                      opacity: 1,
+                    },
+                  },
+                }}
+                onClick={() => {
+                  if (id !== "home") alert("Will Be Available Soon");
+                  setShowMenu(false);
+                }}
+                variant="text"
+              >
+                <CustomTypography variant="h1" fontWeight={"bold"}>
+                  {e.text}
+                </CustomTypography>
+              </CustomButton>
+            );
+          })}
+        </div>
+      </NavbarOptions>
       <Options
         sx={{
-          "&:hover": {
-            ".MuiTypography-root": {
-              opacity: 0.3,
-              transition: "all .2s",
-            },
+          // "&:hover": {
+          ".MuiTypography-root": {
+            opacity: 0.3,
+            transition: "all .2s",
           },
+          // },
           transition: "all .2s",
         }}
       >
@@ -90,10 +154,49 @@ const LogoContainer = styled("div")({
   },
 });
 
-const Options = styled("div")({
+const Options = styled("div")(({ theme }) => ({
   width: "max-content",
   display: "flex",
   flexDirection: "row",
   alignItems: "center",
   justifyContent: "center",
+  [theme.breakpoints.down(880)]: {
+    display: "none",
+  },
+}));
+
+const Navbar = styled("div")(({ theme }) => ({
+  width: "max-content",
+  height: "max-content",
+  [theme.breakpoints.up(880)]: {
+    display: "none",
+  },
+}));
+
+const NavbarOptions = styled("div")({
+  width: "100%",
+  height: "calc(var(--vh, 1vh) * 100)",
+  position: "absolute",
+  top: 0,
+  left: 0,
+  backgroundColor: "#0d0d0d",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  ".options__container": {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  ".header__button__close": {
+    display: "flex",
+    paddingRight: "32px",
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    height: "100px",
+    width: "100%",
+  },
 });
